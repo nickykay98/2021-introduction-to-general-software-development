ecb9cfd703e095d5b965aea15690ac013139d2a3
Nicholas Kondal <nicholas.kondal@gmail.com>
2021-07-12 20:59:36 +1200

    Make counter

--- web-app/react-template/src/components/Counter.tsx
+++ web-app/react-template/src/components/Counter.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Counter.css"
 
 const Counter: React.FC = () => {
-    const count = 5;
+    const [count, setCount] = useState(5);
+
+    const decrement = () => {
+        setCount(count - 1);
+        console.log(count);
+    }
+
+    const increment = () => {
+        setCount(count + 1);
+        console.log(count);
+    }
 
     return <div className="counter-wrapper">
         <div className="counter">
-            <button className="counter-btn">
+            <button className="counter-btn" onClick={decrement}>
                 {"<"}
             </button>
             <div className="counter-label">
                 {`Counter Value:  ${count}`}
             </div>
-            <button className="counter-btn">
+            <button className="counter-btn" onClick={increment}>
                 {">"}
             </button>
         </div>
